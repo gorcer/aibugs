9a5ab878f94000569b99efaa6fe2dc8279a3c4f1
Egor Zarecky <gorcer@gmail.com>
2026-02-27 12:11:08 +1000

    refactor: обновить баланс характеристик и потребления энергии жука

--- src/models/Bug.js
+++ src/models/Bug.js
@@ -8,16 +8,16 @@ class Bug {
         this.y = y;
         this.angle = angle; // 0, 90, 180, 270
         
-        this.current_energy = 10000;
+        this.current_energy = 100;
         this.current_health = 100;
         this.faction = 1;
         this.max_speed = 1;
         this.rotate_speed = 90;
-        this.max_energy = 10000;
-        this.energy_consumption_per_cell = 10;
+        this.max_energy = 100;
+        this.energy_consumption_per_cell = 2;
         this.energy_consumption_per_degree = 0.1;
         this.energy_consumption_per_turn = 1;
-        this.speed_multiply_on_low_energy = 0.3;
+        this.speed_multiply_on_low_energy = 0.5;
         this.max_health = 100;
         this.health_subtract_on_low_energy_per_turn = 1;
         this.health_increase_on_high_energy_per_turn = 1;
@@ -25,7 +25,7 @@ class Bug {
         this.energy_low_amount = 10; // %
         this.attack = 1;
         this.defence = 0;
-        this.feed_speed = 1;
+        this.feed_speed = 10;
         this.visible_range = 5; // дефолтное значение
         this.age = 0;
         this.memory_limit = 10;
